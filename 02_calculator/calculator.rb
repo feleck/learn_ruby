ba57236500e991f2e4c39f736528e32a1e847237
Marcin F <feleck@poczta.onet.pl>
2017-01-03 10:14:56 +0100

    calulator sum, subtraction, multiply,factorial, power

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  sum = 0
+  array.map {|el| sum += el}
+  sum
+end
+
+def multiply(array)
+  multi = 1
+  array.map { |el| multi *= el }
+  multi
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(a)
+  if a == 0
+    return 1
+  else
+    return a * factorial(a-1)
+  end
+end
