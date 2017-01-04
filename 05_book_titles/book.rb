97c73110f9706fa9bfb4760b229ee8f265e807fa
Marcin F <feleck@poczta.onet.pl>
2017-01-04 13:37:25 +0100

    abook titles titleized

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  attr_accessor :title
+  def title
+    words_no_cap = ["and", "in", "of", "or", "the", "over", "to", "the", "a", "an", "but"]
+    result = []
+    title = @title.split
+    title.each_index do |i|
+      title[i] = title[i].capitalize unless words_no_cap.include? title[i]
+      title[i] = title[i].capitalize if title[i] == 'i'
+    end
+    title.first.capitalize!
+    title.join(' ')
+  end
+      
 end
