65d8b54adcd82205253ee184f2f012fb2bbd21ac
Marcin F <feleck@poczta.onet.pl>
2017-01-05 08:22:00 +0100

    atimer 2 versions

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,41 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+  def seconds
+    @seconds
+  end
+  def time_string2
+    minutes = 0
+    hours = 0
+    if @seconds >= 60
+      minutes = @seconds / 60
+      @seconds = @seconds % 60
+    end
+    if minutes >= 60
+      hours = minutes / 60
+      minutes = minutes % 60
+    end
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+  
+  def time_string
+    hours = @seconds / 3600
+    @seconds = @seconds % 3600
+    minutes = @seconds / 60
+    @seconds = @seconds % 60
+    "%02d:%02d:%02d" %[hours, minutes, @seconds]
+  end
+
+  private
+  def check(time)
+  end
+  def padded(time)
+    if time <= 9
+      return "0#{time}"
+    else
+      return time
+    end
+  end
 end
